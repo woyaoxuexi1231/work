ad9ac9ecbc2c6ee352545bc57f83d366c0c481aa
hulei <154347188@qq.com>
2026-05-28 23:37:37 +0800

    feat

---
+++ spring/05-27/spring-interview-qa/src/main/java/com/example/springqa/Q16_MvcRequestFlow/ApiResponse.java
@@ -0,0 +1,29 @@
+package com.example.springqa.Q16_MvcRequestFlow;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+    // 构造方法、getter/setter 省略...
+    
+    public static <T> ApiResponse<T> success(T data) {
+        ApiResponse<T> resp = new ApiResponse<>();
+        resp.setCode(200);
+        resp.setMessage("success");
+        resp.setData(data);
+        return resp;
+    }
+    
+    public static ApiResponse<?> error(int code, String msg) {
+        ApiResponse<?> resp = new ApiResponse<>();
+        resp.setCode(code);
+        resp.setMessage(msg);
+        return resp;
+    }
+}
