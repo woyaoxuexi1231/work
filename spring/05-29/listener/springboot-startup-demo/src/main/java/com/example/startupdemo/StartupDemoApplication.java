83a8c84885c41af5ddc8d6710d6470207d807096
hulei <154347188@qq.com>
2026-05-29 19:05:45 +0800

    feat

--- spring/05-29/listener/springboot-startup-demo/src/main/java/com/example/startupdemo/StartupDemoApplication.java
+++ spring/05-29/listener/springboot-startup-demo/src/main/java/com/example/startupdemo/StartupDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartupDemoApplication {
 
     public static void main(String[] args) {
+        args = new String[]{"--spring.profiles.active=dev"};
         SpringApplication.run(StartupDemoApplication.class, args);
     }
 }
