29f56e36f1d90cfd7f2f8bacc68306a166786f58
hulei <154347188@qq.com>
2026-06-12 15:54:55 +0800

    feat

--- bash/ps/install_kafka.ps1
+++ bash/ps/install_kafka.ps1
@@ -10,11 +10,13 @@ if (check_container_exists $Container) { exit 0 }
 cleanup_container $Container
 New-Item -ItemType Directory -Force -Path $Data | Out-Null
 
+$HostIp = if ($env:KAFKA_HOST) { $env:KAFKA_HOST } else { "localhost" }
+
 pull_image $Image
 docker run -d --name $Container --restart unless-stopped -p ${Port}:9092 -e TZ=Asia/Shanghai `
   -e KAFKA_NODE_ID=1 -e KAFKA_PROCESS_ROLES=broker,controller `
   -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093 `
-  -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 `
+  -e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://${HostIp}:${Port}" `
   -e KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER -e KAFKA_CONTROLLER_QUORUM_VOTERS=1@localhost:9093 `
   -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 `
   -v "${Data}:/var/lib/kafka/data" `
